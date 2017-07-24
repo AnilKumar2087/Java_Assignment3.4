ac514a51da118cc9f34243ba7ba7d7ac0a61593d
AnilKumar2087 <a.anil.adunuri@gmail.com>
2017-07-24 23:28:22 +0530

    changes

---
+++ src/session3_assignment4/StudentResultProcessing.java
@@ -0,0 +1,12 @@
+package session3_assignment4;
+
+public class StudentResultProcessing extends Student {
+
+	public static void main(String[] args) {
+			
+			Student k = new Student();
+			k.result();
+			
+		}
+		
+}
